789c0277cd1956d12c9c9c8093653b681e8f1ddc
ibmalone <ibmalone@gmail.com>
2023-06-03 15:14:15 +0100

    Add command line arguments to control speed and rating filters, update README.md to reflect usage. Add ECO and opening name to reporting.

--- README.md
+++ README.md
@@ -12,11 +12,11 @@ The idea is to be able to take an opening sequence and produce data such as win/
 | blitz | 2000   | 47    | 48    |
 | ...   |        |       |       |
 
-Currently pgn-win-rates.py will query entries in a pgn file (test.pgn), query Lichess's master and player databases, and report its results (game counts for white win, draw, black win) for each mainline position found. If requests trigger the servers rate limit it will wait before continuing.
+Currently pgn-win-rates.py will query entries in a pgn file (-p arguemnt), query Lichess's master and player databases, and report its results (game counts for white win, draw, black win) for each mainline position found. If requests trigger the servers rate limit it will wait before continuing. The ratings and speeds to query in the player database can be controlled by -r and -s options.
 
 Results look like this:
-1. e4 e5 2. Nf3 Nc6 3. Bc4,lichess,blitz,1600,10339727,791029,9225226
-1. e4 e5 2. Nf3 Nc6 3. Bc4,lichess,blitz,1800,6855871,584130,6239764
-1. e4 e5 2. Nf3 Nc6 3. Bc4,lichess,blitz,2000,2863512,286746,2703096
+1. e4 e5 2. Nf3 Nc6 3. Bc4,C50,Italian Game,lichess,blitz,1200,9274322,640141,8174457
+1. e4 e5 2. Nf3 Nc6 3. Bc4,C50,Italian Game,lichess,blitz,1400,11495424,820170,10219813
+1. e4 e5 2. Nf3 Nc6 3. Bc4,C50,Italian Game,lichess,rapid,1200,3928876,290786,3333522
 
 If you find yourself making serious use of this you may wish to consider donating to [lichess](https://lichess.org/) towards their server costs. If you find yourself making heavy use then you may want to consider hosting your own private lila-opening explorer instance, as it may well be faster than making queries to lichess's servers.
--- pgn-win-rates.py
+++ pgn-win-rates.py
@@ -30,11 +30,25 @@ onlyspeeds = ["blitz","rapid"]
 parser = argparse.ArgumentParser(
     prog="pgn-win-rates.py",
     description="pull winning rates for pgn variations from a lila-openingexplorer host")
-parser.add_argument("-p","--pgn",required=True,type=str)
+parser.add_argument("-p","--pgn", required=True,type=str, metavar="PGN",
+                    help="Input PGN file. N.B. games should be separated by"+
+                    " a double newline.")
+parser.add_argument("-s","--speeds",default=None,type=str, metavar="SPEEDS",
+                    help="Comma-separated list of time controls to retrieve"+
+                    " allowed values "+
+                    "ultraBullet,bullet,blitz,rapid,classical,correspondence")
+parser.add_argument("-r","--ratings",default=None,type=str, metavar="RATINGS",
+                    help="Comma-separated list of ratings to retrieve"+
+                    " allowed values "+
+                    "0,1000,1200,1400,1600,1800,2000,2200,2500")
 
 args = parser.parse_args()
 
 pgn = open(args.pgn)
+if args.speeds is not None:
+    onlyspeeds = args.speeds.split(",")
+if args.ratings is not None:
+    onlyratings = [ratings for ratings in map(int,args.ratings.split(","))]
 
 
 
@@ -58,6 +72,7 @@ for game in iter(partial(chess.pgn.read_game,pgn),None) :
     ucilist = [move.uci() for move in movelist]
     sanstr = board.variation_san(movelist)
     ucistr = ",".join(ucilist)
+    opening = None
     for dbname, db in dbs.items():
         for speeds in db["speeds"]:
             if (onlyspeeds is not None and speeds is not None and
@@ -69,6 +84,8 @@ for game in iter(partial(chess.pgn.read_game,pgn),None) :
                     continue
                 reqdat = getEntryJSON(db['url'],speeds,
                                       ratings,ucistr)
+                if opening is None:
+                    opening = reqdat['opening']
                 report = ",".join(map(str,
-                                       [sanstr,dbname,speeds,ratings,reqdat['white'],reqdat['draws'],reqdat['black']]))
+                                       [sanstr,opening['eco'],opening['name'],dbname,speeds,ratings,reqdat['white'],reqdat['draws'],reqdat['black']]))
                 print(report)
