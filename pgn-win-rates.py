b34a2219f6fc46158c277c1cbf58b697173e3016
ibmalone <ibmalone@gmail.com>
2023-06-04 18:18:17 +0100

    Use pandas for result handling and csv output, separate game listing and result compilation steps, plan is to make future addition of variation traversal easier. Add a --test option to avoid querying the database while working on output processing.

--- pgn-win-rates.py
+++ pgn-win-rates.py
@@ -7,6 +7,7 @@ import requests
 
 import chess.pgn
 import chess
+import pandas as pd
 
 retrytime=60
 
@@ -41,6 +42,11 @@ parser.add_argument("-r","--ratings",default=None,type=str, metavar="RATINGS",
                     help="Comma-separated list of ratings to retrieve"+
                     " allowed values "+
                     "0,1000,1200,1400,1600,1800,2000,2200,2500")
+parser.add_argument("-o","--out",default=None,type=str, metavar="CSV",
+                    help="Optional csv file to save to")
+parser.add_argument("-t","--test",action='store_true',
+                    help="Test mode, use dummy data rather than "+
+                    "querying server.")
 
 args = parser.parse_args()
 
@@ -65,13 +71,26 @@ def getEntryJSON(url, speeds, ratings, ucistr):
             req.raise_for_status()
     return req.json()
 
+if args.test:
+    def getEntryJSON(url, speeds, ratings, ucistr):
+        return {
+            "white":3007703,"draws":294719,"black":2648993,
+            "opening":{"eco":"D10","name":"Slav Defense: Exchange Variation"}
+        }
 
+# get a list of game ucis to query
+seqlist = []
 for game in iter(partial(chess.pgn.read_game,pgn),None) :
     board = game.board()
     movelist = [ move for move in game.mainline_moves()]
     ucilist = [move.uci() for move in movelist]
     sanstr = board.variation_san(movelist)
-    ucistr = ",".join(ucilist)
+    uci = ",".join(ucilist)
+    seqlist.append({'uci':uci, 'san':sanstr})
+
+# print results for each uci in the list
+records =[]
+for seq in seqlist :
     opening = None
     for dbname, db in dbs.items():
         for speeds in db["speeds"]:
@@ -83,9 +102,20 @@ for game in iter(partial(chess.pgn.read_game,pgn),None) :
                     ratings not in onlyratings):
                     continue
                 reqdat = getEntryJSON(db['url'],speeds,
-                                      ratings,ucistr)
+                                      ratings,seq['uci'])
                 if opening is None:
                     opening = reqdat['opening']
-                report = ",".join(map(str,
-                                       [sanstr,opening['eco'],opening['name'],dbname,speeds,ratings,reqdat['white'],reqdat['draws'],reqdat['black']]))
-                print(report)
+                record = {'san':seq['san'], 'eco':opening['eco'],
+                          'opening':opening['name'],'dbname':dbname,
+                          'speeds':speeds,'ratings':ratings,
+                          'whitewins':reqdat['white'],'draws':reqdat['draws'],
+                          'blackwins':reqdat['black']}
+                records.append(record)
+
+recordsDF = pd.DataFrame.from_records(records)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+print(recordsDF)
+if (args.out is not None):
+    recordsDF.to_csv(args.out,index=False)
